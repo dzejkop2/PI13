7a001dc2638f0f25247fe1f91bba680b9fba6204
Jakub Geleta <mrkuka403@gmail.com>
2023-10-06 11:54:17 +0200

    OOP

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-for i in range(30):
-    print("Hello kupko")
+import datetime
+
+
+class Osoba:
+    def __init__(self, meno_param, priezvisko_param, rok_param):
+        self.meno = meno_param
+        self.priezvisko = priezvisko_param
+        self.rok = rok_param
+        self.vek = datetime.date.today().year - self.rok
+
+    def pozdrav(self):
+        print(f"Ahoj, ja som {self.meno} {self.priezvisko} a mám {self.vek} rokov")
+
+    def vypis_vek(self):
+        print(self.vek)
+
+
+kupko = Osoba("Jakub", "Geleta", 2006)
+jano = Osoba("Janko", "Hruška", 2000)
+
+kupko.pozdrav()
+kupko.vypis_vek()
+jano.pozdrav()
+jano.vypis_vek()
