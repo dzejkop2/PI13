a3aef92e81da2e14d235b8d69e856ef1f7c9c1c2
Jakub Geleta <mrkuka403@gmail.com>
2023-10-13 11:44:42 +0200

    studenti a ucitelia loliky lol

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 
 
 class Osoba:
@@ -16,19 +17,82 @@ class Osoba:
 
 
 class Ucitel(Osoba):
-    def __init__(self, meno, priezvisko, rok, titul, predmet):
+    def __init__(self, meno, priezvisko, rok, titul, predmet, trieda):
         Osoba.__init__(self,meno,priezvisko,rok)
         self.titul = titul
         self.predmet = predmet
+        self.trieda = trieda
     def pozdrav(self):
-        print(f"Dobrý deň, som učiteľ {self.titul} {self.meno} {self.priezvisko} a mám {self.vek} rokov, učím predmet {self.predmet}")
+        print(f"Dobrý deň, som učiteľ {self.titul} {self.meno} {self.priezvisko} a mám {self.vek} rokov, učím predmet {self.predmet} a som tiredny triedy {self.trieda}")
 
 
-jozo = Ucitel("Jozef","Hruška",1995, "Ing.", "Programovanie")
-jano = Osoba("Ján","Jablko",2004)
+
+class Student(Osoba):
+    def __init__(self, meno, priezvisko, rok, trieda):
+        super().__init__(meno, priezvisko, rok)
+        self.trieda = trieda
+
+    def pozdrav(self):
+        print(f"Ahoj, volam sa {self.meno} {self.priezvisko} a mám {self.vek} rokov a som žiakom triedy {self.trieda}")
 
 
-jozo.pozdrav()
-jozo.vypis_vek()
-jano.pozdrav()
-jano.vypis_vek()
+POCET_STUDENTOV = 10
+POCET_UCITELOV = 5
+
+studenti = list()
+
+for i in range(POCET_STUDENTOV):
+    with open("mena.txt", "r", encoding="utf-8") as t:
+        mena = tuple(t)
+
+    with open("priezviska.txt", "r", encoding="utf-8") as p:
+        priezviska = tuple(p)
+
+    meno = random.choice(mena)
+    meno = meno[:-1]
+
+    priezvisko = random.choice(priezviska)
+    priezvisko = priezvisko[:-1]
+
+    rok = random.randint(2005,2008)
+    if rok == 2005:
+        trieda = "IV."
+    elif rok == 2006:
+        trieda = "III."
+    elif rok == 2007:
+        trieda = "II."
+    else:
+        trieda = "I."
+
+    trieda += random.choice(("A","B","C"))
+
+    studenti.append(Student(meno,priezvisko,rok,trieda))
+
+print("Študenti: \n")
+for i in range(POCET_STUDENTOV):
+    studenti[i].pozdrav()
+
+ucitelia = list()
+for i in range(POCET_UCITELOV):
+    with open("mena.txt", "r", encoding="utf-8") as t:
+        mena = tuple(t)
+
+    with open("priezviska.txt", "r", encoding="utf-8") as p:
+        priezviska = tuple(p)
+
+    meno = random.choice(mena)
+    meno = meno[:-1]
+
+    priezvisko = random.choice(priezviska)
+    priezvisko = priezvisko[:-1]
+
+    rok = random.randint(1958, 2004)
+    predmet = random.choice(("Matematika","Fyzika","Programovanie"))
+    titul = random.choice(("Mgr.","Ing.","Bc."))
+    trieda = random.choice(("I.","II.","III.","IV.")) + random.choice(("A","B","C"))
+
+    ucitelia.append(Ucitel(meno, priezvisko, rok, titul, predmet, trieda))
+
+print("Učitelia: \n")
+for i in range(POCET_UCITELOV):
+    ucitelia[i].pozdrav()
